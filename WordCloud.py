68fe2c0229592cbb415dbbd6010fb62a7d8d7e77
K Vadhiraja <33854697+Vadhiraja@users.noreply.github.com>
2020-07-10 21:13:59 +0530

    Add files via upload

---
+++ WordCloud.py
@@ -0,0 +1,66 @@
+
+# Here are all the installs and imports you will need for your word cloud script and uploader widget
+
+get_ipython().system('pip install wordcloud')
+get_ipython().system('pip install fileupload')
+get_ipython().system('pip install ipywidgets')
+get_ipython().system('jupyter nbextension install --py --user fileupload')
+get_ipython().system('jupyter nbextension enable --py fileupload')
+
+import wordcloud
+import numpy as np
+from matplotlib import pyplot as plt
+from IPython.display import display
+import fileupload
+import io
+import sys
+
+
+def _upload():
+
+    _upload_widget = fileupload.FileUploadWidget()
+
+    def _cb(change):
+        global file_contents
+        decoded = io.StringIO(change['owner'].data.decode('utf-8'))
+        filename = change['owner'].filename
+        print('Uploaded `{}` ({:.2f} kB)'.format(
+            filename, len(decoded.read()) / 2 **10))
+        file_contents = decoded.getvalue()
+
+    _upload_widget.observe(_cb, names='data')
+    display(_upload_widget)
+
+_upload()
+
+
+
+def calculate_frequencies(file_contents):
+    # Here is a list of punctuations and uninteresting words you can use to process your text
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my",     "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them",     "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being",     "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how",     "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"]
+    
+    frequency = {}
+    done = []
+    for pu in punctuations:
+        file_contents.replace(pu,'')
+    for word in uninteresting_words:
+        w = ' '+word+' '
+        file_contents.replace(w,'')
+    for words in file_contents.split():
+        if words.lower() not in done:
+            done.append(word.lower())
+            if words not in frequency:
+                frequency[words]=1
+            else:
+                frequency[words]+=1
+    
+    #wordcloud
+    cloud = wordcloud.WordCloud()
+    cloud.generate_from_frequencies(frequency)
+    return cloud.to_array()
+
+myimage = calculate_frequencies(file_contents)
+plt.imshow(myimage, interpolation = 'nearest')
+plt.axis('off')
+plt.show()
